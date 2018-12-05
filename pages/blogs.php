61d9cb9307a889e68c10856899cb0397103ffaca
Basit Mir <iammirbasit@gmail.com>
2018-12-05 16:22:24 +0530

    minute changes

--- pages/blogs.php
+++ pages/blogs.php
@@ -1,46 +1,66 @@
 <?php
   require 'header.php'
   ?>
-
+<div class="bg">
 <div class="header">
+
   <h2>Blog Name</h2>
 </div>
 
 <div class="row">
   <div class="leftcolumn">
+  <div class="card">
+  <form>
+  <div class="form-group">
+    <label for="exampleFormControlInput1">Name</label>
+    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Basit Mir">
+  </div>
+  <div class="form-group">
+    <label for="exampleFormControlInput2">Email</label>
+    <input type="email" class="form-control" id="exampleFormControlInput2" placeholder="iammirbasit@gmail.com">
+  </div>
+  
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1">Start Blogging</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+  </div>
+  </div>
+  <p class="lead">
+    <button type="submit" class=" form-control btn btn-outline-secondary btn-lg"  role="button">PUBLISH</button>
+  </p>
+</form>
+
+
+ 
+
+
     <div class="card">
       <h2>TITLE HEADING</h2>
       <h5>Title description, Dec 7, 2017</h5>
       <div class="fakeimg" style="height:200px;">Image</div>
       <p>Some text..</p>
     </div>
-    <div class="card">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Sep 2, 2017</h5>
-      <div class="fakeimg" style="height:200px;">Image</div>
-      <p>Some text..</p>
-    </div>
+
   </div>
   <div class="rightcolumn">
-    <div class="card">
-      <h2>About Me</h2>
-      <div class="fakeimg" style="height:100px;">Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-    </div>
+    
     <div class="card">
       <h3>Popular Post</h3>
       <div class="fakeimg">Image</div><br>
       <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
+      <div class="fakeimg">Image</div><br>
       <div class="fakeimg">Image</div>
     </div>
-    <div class="card">
-      <h3>Follow Me</h3>
-      <p>Some text..</p>
-    </div>
+    
   </div>
 </div>
 
-
+</div>
 
 
 
